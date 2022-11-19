eb5eaf031da647a80dcc7f4a034bb47b1b6171aa
Gunawan Wahyu <greezyzilla@gmail.com>
2022-11-19 16:52:41 +0700

    fix: :bug: fix comment body not parsed correctly and migrate scale to margin

--- components/organisms/threadDetail.tsx
+++ components/organisms/threadDetail.tsx
@@ -54,9 +54,9 @@ export default function ThreadDetails({ thread }: ThreadDetailProps) {
       </div>
       <div className="ml-3 mt-5">
         <h2 className="font-semibold text-slate-600 mb-2">Comments</h2>
-        <div className="flex flex-col ">
+        <div className="flex flex-col gap-4 mr-8">
           {thread.comments.map((comment) => (
-            <div key={comment.id} className="p-5 scale-90 origin-top-left rounded-lg flex flex-col gap-2 bg-white">
+            <div key={comment.id} className="p-5 rounded-lg flex flex-col gap-2 bg-white">
               <div className="flex gap-3">
                 <img src={comment.owner.avatar} className="w-10 h-10 rounded-lg" alt={comment.owner.name} />
                 <div className="flex flex-col justify-center">
@@ -67,7 +67,7 @@ export default function ThreadDetails({ thread }: ThreadDetailProps) {
                 </div>
               </div>
               <p className="text-slate-600/80 leading-6 text-sm">
-                {comment.content}
+                {parse(comment.content)}
               </p>
               <div className="flex gap-4">
                 <div className="p-2 gap-1 text-xs bg-slate-100/80 rounded-lg flex items-center justify-center">
