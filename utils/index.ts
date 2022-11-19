223280a4811b9fbeaa6aee57788fa91f203ef541
Gunawan Wahyu <greezyzilla@gmail.com>
2022-11-19 17:30:46 +0700

    fix: :bug: fix relative date sometimes undefined

--- utils/index.ts
+++ utils/index.ts
@@ -1,5 +1,10 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayjs.extend(relativeTime);
+
 export function getAccessToken() {
   const accessToken = localStorage.getItem('accessToken');
   return accessToken;
@@ -46,22 +51,4 @@ export async function fetcWithoutToken(url : string, options : AxiosRequestConfi
   }
 }
 
-export const getRelativeDate = (dateString: string) => {
-  const delta = Math.round((+new Date() - +new Date(dateString)) / 1000);
-
-  const minute = 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  let result;
-
-  if (delta < 30) result = 'just then.';
-  else if (delta < minute) result = `${delta} seconds ago.`;
-  else if (delta < 2 * minute) result = 'a minute ago.';
-  else if (delta < hour) result = `${Math.floor(delta / minute)} minutes ago.`;
-  else if (Math.floor(delta / hour) === 1) result = '1 hour ago.';
-  else if (delta < day) result = `${Math.floor(delta / hour)} hours ago.`;
-  else if (delta < day * 2) result = 'yesterday';
-
-  return result;
-};
+export const getRelativeDate = (dateString: string) => dayjs(dateString).fromNow();
